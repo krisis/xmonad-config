e5b025ea1ece92533287daa14a6e7099a2fc59f6
Krishnan Parthasarathi <kp@minio.io>
2016-05-02 09:18:52 +0530

    Moved to DynNetwork plugin
    Signed-off-by: Krishnan Parthasarathi <kp@minio.io>

--- xmobar.hs
+++ xmobar.hs
@@ -12,12 +12,18 @@ Config {
     commands = [
         Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        --Run Network "wlan0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run DynNetwork     [ "--template" , "<dev>: <tx>kB/s|<rx>kB/s"
+                             , "--Low"      , "1000"       -- units: kB/s
+                             , "--High"     , "5000"       -- units: kB/s
+                             , "--low"      , "darkgreen"
+                             , "--normal"   , "darkorange"
+                             , "--high"     , "darkred"
+                             ] 10,
+
         Run Date "%a %b %_d %l:%M" "date" 10,
         Run StdinReader
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{ %multicpu%   %memory%   %swap%   %em1%   <fc=#FFFFCC>%date%</fc>   %KPAO%"
+    template = "%StdinReader% }{ %multicpu%   %memory%   %dynnetwork%   <fc=#FFFFCC>%date%</fc>   %KPAO%"
 }
